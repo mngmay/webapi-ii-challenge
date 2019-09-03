77ea2ee46a4631fdd372a861b4b0776a699e04de
May Ng <mngmay@gmail.com>
2019-09-03 14:32:29 -0700

    Created PUT requests

--- server.js
+++ server.js
@@ -136,4 +136,38 @@ server.delete("/api/posts/:id", (req, res) => {
     });
 });
 
+server.put("/api/posts/:id", (req, res) => {
+  const postId = req.params.id;
+  const changes = req.body;
+
+  if (!changes.title || !changes.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+  Posts.update(postId, changes)
+    .then(updated => {
+      if (updated) {
+        Posts.findById(postId)
+          .then(post => {
+            res.status(200).json(changes);
+          })
+          .catch(error => {
+            res
+              .status(500)
+              .json({ message: "The post informaiton could not be modified" });
+          });
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "The post informaiton could not be modified" });
+    });
+});
+
 module.exports = server;
